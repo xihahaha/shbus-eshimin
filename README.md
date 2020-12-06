04a79e7356d89d7e39f6953cd85d71e76d0d9cca
系哈哈 <x_d_n_8@sina.com>
2020-12-06 14:35:32 +0800

    修复浦东公交站点无法识别问题

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # shbus-eshimin
- 
--- bus.py
+++ bus.py
@@ -28,34 +28,36 @@ def getBus(line_name):
     result = line_name + '：' + busbase['start_stop'] + ' >>> ' + busbase['end_stop'] + '\n'
     result += '首班车：' + busbase['start_earlytime'] + '  末班车：' + busbase['start_latetime'] + '\n'
     for i in range(l0):
-        url2 = "http://apps.eshimin.com/traffic/gjc/getArriveBase?name=" + name + '&lineid=' + line_id + '&direction=0&stopid=' + str(i)
+        index = busstop['lineResults0']['stops'][i]['id']
+        index = index.replace('start_', '')
+        url2 = "http://apps.eshimin.com/traffic/gjc/getArriveBase?name=" + name + '&lineid=' + line_id + '&direction=0&stopid=' + str(index)
         req2 = urllib.request.Request(url2, headers=headers)
         data2 = urllib.request.urlopen(req2)
         arrive_base = data2.read().decode('utf-8')
         arrivebase = json.loads(arrive_base)
         if arrivebase:
             if arrivebase['cars'][0]['stopdis'] == '1':
-                result += arrivebase['cars'][0]['terminal'] + ' 距离 ' + busstop['lineResults0']['stops'][i][
-                    'zdmc'] + ' ' + arrivebase['cars'][0]['distance'] + ' 米 ' + arrivebase['cars'][0][
-                              'time'] + ' 秒 ' + '\n'
+                result += arrivebase['cars'][0]['terminal'] + ' 距离 ' + busstop['lineResults0']['stops'][i]['zdmc'] + ' ' + arrivebase['cars'][0]['distance'] + ' 米 ' + arrivebase['cars'][0]['time'] + ' 秒 ' + '\n'
     if busstop['lineResults1']['stops']:
         l1 = len(busstop['lineResults1']['stops'])
         result += '\n'
         result += line_name + '：' + busbase['end_stop'] + ' >>> ' + busbase['start_stop'] + '\n'
         result += '首班车：' + busbase['end_earlytime'] + '  末班车：' + busbase['end_latetime'] + '\n'
         for i in range(l1):
-            url3 = "http://apps.eshimin.com/traffic/gjc/getArriveBase?name=" + name + '&lineid=' + line_id + '&direction=1&stopid=' + str(i)
+            index = busstop['lineResults1']['stops'][i]['id']
+            index = index.replace('start_', '')
+            url3 = "http://apps.eshimin.com/traffic/gjc/getArriveBase?name=" + name + '&lineid=' + line_id + '&direction=1&stopid=' + str(index)
             req3 = urllib.request.Request(url3, headers=headers)
             data3 = urllib.request.urlopen(req3)
             arrive_base1 = data3.read().decode('utf-8')
             arrivebase1 = json.loads(arrive_base1)
             if arrivebase1:
                 if arrivebase1['cars'][0]['stopdis'] == '1':
-                    result += arrivebase1['cars'][0]['terminal'] + ' 距离 ' + busstop['lineResults0']['stops'][i]['zdmc'] + ' ' + arrivebase1['cars'][0]['distance'] + ' 米 ' + arrivebase1['cars'][0]['time'] + ' 秒 ' + '\n'
+                    result += arrivebase1['cars'][0]['terminal'] + ' 距离 ' + busstop['lineResults1']['stops'][i]['zdmc'] + ' ' + arrivebase1['cars'][0]['distance'] + ' 米 ' + arrivebase1['cars'][0]['time'] + ' 秒 ' + '\n'
     return result
 
 
 if __name__ == '__main__':
-    line = '312路'
+    line = '454路'
     result = getBus(line)
     print(result)
